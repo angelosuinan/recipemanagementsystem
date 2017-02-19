c1a1f951ee33f686ac92716d45e13488c6234660
angeosuinan <suinanangelo@gmail.com>
2017-02-19 21:56:40 +0800

    added class based views

--- recipemanagement/home/views.py
+++ recipemanagement/home/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import View
 
-# Create your views here.
+class Index(View):
+        """
+        https://docs.djangoproject.com/en/1.9/topics/class-based-views/intro/
+        """
+        template_name = 'home/index.html'
 
-from django.http import HttpResponse
+        def get(self, request, *args, **kwargs):
+            return render(request, self.template_name,)
 
-
-def index(request):
-    return HttpResponse("TEST")
+        def post(self, request, *args, **kwargs):
+            pass
