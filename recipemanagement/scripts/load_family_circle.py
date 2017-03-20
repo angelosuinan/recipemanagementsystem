727a9983bfff0d5af5f301f7e181ae74af2f2e1c
angeosuinan <suinanangelo@gmail.com>
2017-03-20 09:20:30 +0800

    updated scripts

--- recipemanagement/scripts/load_family_circle.py
+++ recipemanagement/scripts/load_family_circle.py
@@ -9,23 +9,27 @@ dataReader
 
 import random
 for row in dataReader:
-    recipe = Recipe()
     if row[0] == 'direction':
+        print ("SDFDFAS")
         continue
-    my_list = row[4].split(",")
-    for pr in my_list:
-        try:
-            product = Product()
-            product.name = pr
-            product.price =  round(random.uniform(10.00,500),2)
-            product.save()
-            recipe.product = product
-        except django.db.utils.IntegrityError:
-            pass
+    recipe = Recipe()
     recipe.name = row[1]
     recipe.img_url = row[3]
     recipe.prep_time = row[8]
     recipe.servings = row[7]
     recipe.directions = row[0]
     recipe.ingredients = row[2]
+    my_list = row[4].split(",")
+    recipe.save()
+    for pr in my_list:
+        try:
+            product = Product()
+            product.name = pr
+            product.price =  round(random.uniform(10.00,500),2)
+            product.save()
+            p = Product.objects.get(name=product.name)
+            recipe.products.add(p)
+        except django.db.utils.IntegrityError:
+            old = Product.objects.get(name=pr)
+            recipe.products.add(old)
     recipe.save()
